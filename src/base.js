0ad64b30125689a0408958ef58da77acb3f8c19d
Scott Vandehey <fake-scott@spaceninja.com>
2018-11-06 16:57:16 -0800

    Use .env Files for Separate Dev & Prod Database
    By moving the Firebase config variables to `.env`, they can be
    overridden locally using `.env.local`, allowing for a different
    database on prod vs local development.
    
    Fixes #23

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 export const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyC8ZnoxnKQPD43YTsgAKyWQdWvPQ5rbujM",
-  authDomain: "spaceninja-book-list.firebaseapp.com",
-  databaseURL: "https://spaceninja-book-list.firebaseio.com"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 });
 
 const base = Rebase.createClass(firebaseApp.database());
