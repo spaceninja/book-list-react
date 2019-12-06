1a09a4bc354dff1d670d5941f885a97b58971101
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-06 06:20:17 +0000

    Deploy spaceninja/book-list-react to github.com/spaceninja/book-list-react.git:gh-pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "normalize.css";
-import "./scss/main.scss";
-
-ReactDOM.render(<App />, document.getElementById("root"));
