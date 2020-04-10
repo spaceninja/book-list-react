702ac0936a82e47cb69d7b4ccfdc1bc7200e88d9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-10 18:11:34 +0000

    Deploy spaceninja/book-list-react to github.com/spaceninja/book-list-react.git:gh-pages

---
+++ precache-manifest.920f8f7fed90a3ff91166559c9a37792.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10119b76f9e22f7871f5163bf590290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aecf1229c9bb83087967",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3fc6386595e53d721453",
+    "url": "/static/css/main.e1244006.chunk.css"
+  },
+  {
+    "revision": "aecf1229c9bb83087967",
+    "url": "/static/js/2.b7fc4311.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/static/js/2.b7fc4311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc6386595e53d721453",
+    "url": "/static/js/main.22937035.chunk.js"
+  },
+  {
+    "revision": "06019e2aa271f4e0de9f",
+    "url": "/static/js/runtime-main.32275d4c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f93d23c02e1a5ffb7e0354b6b82af7b5.js"
+  "/precache-manifest.920f8f7fed90a3ff91166559c9a37792.js"
 );
 
 self.addEventListener('message', (event) => {
