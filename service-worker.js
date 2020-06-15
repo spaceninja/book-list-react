6edfb503829409750d85ef38cb5e59934689bc67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-15 17:38:09 +0000

    Deploy spaceninja/book-list-react to github.com/spaceninja/book-list-react.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f172ec594a587066dc147565bcb31fa.js"
+  "/precache-manifest.5ac9859ddd48b2c84f9dfdc8ee60990e.js"
 );
 
 self.addEventListener('message', (event) => {
